9b96b310b39b66c5b3eb718081954d8c01d0db32
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-03-03 13:13:32 +1100

    Add files

---
+++ website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    # Create unique identifier to differentiate different users
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # Foreign key that references a column of another database
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    first_name = db.Column(db.String(150))
+    password = db.Column(db.String(150))
+    notes = db.relationship("Note")
